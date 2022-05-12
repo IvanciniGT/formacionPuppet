582ba30a648f9ab8418e18f185139f0ca8e30626
Ubuntu <ubuntu@ip-172-31-12-98.eu-west-1.compute.internal>
2022-05-12 18:27:37 +0000

    dia4

--- configuraciones/manifests/site.pp
+++ configuraciones/manifests/site.pp
@@ -1,10 +1,36 @@
 
 node default {
 
-    $nombredelservidor = 'curso'
+    # Esto es una mala práctica
+    # No tengo control de estas variables: NO TENGO ENCAPSULACION
+    $nombredelservidor = 'curso' # Me lo llevo a hiera...
+    # Asi puedo tener un nombre distinto para cada nodo
     $correoadministradorsitio = 'micorreo@miempresa.es'
 
     include apache
     include apache::virtualhost
 
+    #mysql
+    # base de datos
+
+    # Para una instalación básica:
+    include mysql::server
+    
+    # Esto SI ES UNA BUENA PORACTICA para inyectar valores a una clase / modulo
+    # OJO, esto requiere que el módulo haya definido esas variables en la calse,
+    # Como nosotros hemos hecho en el fichero init.pp...
+    # Y como no hemos hecho en el fichero visrtualhost, que es una RUINA !!!!
+    #class { 'mysql::server':
+    #  #root_password           => 'strongpassword',
+    #  remove_default_accounts => true,
+    #  restart                 => true,
+    #}
+    
+    mysql::db { 'cursoDB':
+      user     => 'usuario',
+      password => 'password',
+      host     => 'localhost',
+      grant    => ['SELECT', 'UPDATE'],
+    }
+    
 }
--- notas/dia4.md
+++ notas/dia4.md
@@ -17,3 +17,28 @@ puppet agent es el programa que se pone en contacto con el servidor
 y le pide un catalogo y lo ejecuta, previo mandarle los facts
 
 puppet apply... este programa nos permite ejecutar un catalogo directamente en local, sin pasar por servidor
+
+
+En una instalacion real, instalamos r10k.
+
+Esta herramienta requiere un fichero de configuracion: /etc/r10k.yaml 
+:cachedir: '/var/cache/r10k' 
+:sources: 
+:opstree: 
+remote: 'https://github.com/fullstack-puppet/fullstackpuppet-environment.git' 
+basedir: '/etc/puppetlabs/code/environments' 
+
+
+Una vez creado este fichero, ejecutamos:
+$ r10k deploy environment -pv 
+
+Esto sincroniza los ficheros de mi repo, con las carpetas adecuadas del servidor de puppet
+
+
+Normalmente hacemos un cron, para que esto se actualice con una periodicidad X
+
+cat << EOF > /etc/cron.d/r10k.conf 
+SHELL = /bin/bash 
+PATH = /sbin:/bin:/usr/sbin:/usr/bin 
+H/15 * * * * root r10k deploy environment -p 
+EOF
